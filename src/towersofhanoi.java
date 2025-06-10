5294b5ef86d48e06c9b37957246dd92b3218a8fd
Linny <lincolnmwash@gmail.com>
2025-06-10 10:55:09 +0300

    towers of hanoi ( with recursion )

---
+++ src/towersofhanoi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class towersofhanoi {
+    static void hanoiTowers(int n, char fromRod, char toRod, char helperRod){
+        if (n==1){
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod" + toRod);
+            return;
+        }
+        hanoiTowers(n-1,fromRod, helperRod,toRod);
+        System.out.println("Take disk "+ fromRod + " from rod " + fromRod + " to rod " + toRod);
+        hanoiTowers(n-1,fromRod,toRod,helperRod);
+    }
+    public static void main(String[] args){
+        System.out.println("Please enter number of disks: ");
+        Scanner scanner = new Scanner(System.in);
+        int disks= scanner.nextInt();
+        hanoiTowers(disks,'A','B','C');
+    }
+}
